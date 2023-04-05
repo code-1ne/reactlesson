69399714a26f62530b5c97a15735895ac3de763c
Andrey <andreybaran@gmail.com>
2023-04-05 12:23:22 +0300

    1.1

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import Counter from "./components/Counter";
+
+function App() {
+  const [value, setValue] = useState("ТЕКСТ В ИНПУТЕ");
+
+  return (
+    <div className="App">
+      <Counter/>
+      <Counter/>
+      <Counter/>
+      <Counter/>
+      <Counter/>
+    </div>
+  );
+}
+
+export default App;
